2ed3d1a2f7b510ef2fd0eda27deee36ed3d8c3e8
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-02-17 00:29:46 -0500

    adding all my questions and statements

---
+++ index.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        // {
+        //     type: 'input',
+        //     message: 'What is your project title?',
+        //     name: 'title',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'description of your project',
+        //     name: 'description',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'installation instructions',
+        //     name: 'installation',
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'usage information',
+        //     name: 'usage',
+        // },
+        {
+            type: 'input',
+            message: 'contribution guidelines?',
+            name: 'contributing',
+        },
+        {
+            type: 'input',
+            message: 'test insructions',
+            name: 'test',
+        },
+        {
+            type: 'list',
+            message: 'license?',
+            name: 'license',
+            choices: [
+                "MIT",
+                "Apache",
+                "GPL"
+            ]
+        },
+        {
+            type: 'input',
+            message: 'github username',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: 'email',
+            name: 'email',
+        },
+    ]).then((response) =>
+        fs.writeFile(
+            "./README.md" ,
+
+            `
+            # ${response.title}
+
+            ## Description 
+
+            ${response.description}
+
+            ## Installation Instructions
+
+            ${response.installation}
+
+            ## Usage Information 
+
+            ${response.usage}
+
+            ## Contributors 
+
+            ${response.contributing}
+
+            ## Testing Instructions 
+
+            ${response.instructions}
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            ,(err) => err ? console.log("There was an error!") : console.log("Successfully appended to file!")
+
+
+        )
+    );
